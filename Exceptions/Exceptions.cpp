0de4a2d4d1576a44c709503d7a99dd5207539c65
Isaac Booth <TheRealIkeB@gmail.com>
2021-05-02 19:12:44 -0400

    Built the driver for the character function and implemented catch statements for all 3 exceptions

--- Exceptions/Exceptions.cpp
+++ Exceptions/Exceptions.cpp
@@ -3,6 +3,7 @@
 //May 2, 2021
 
 #include <iostream>
+#include <string>
 using namespace std;
 
 //Function prototype
@@ -10,8 +11,30 @@ char character(char start, int offset);
 
 int main()
 {
+	//User input
+	char start = 0, target = 0; int offset = 0;
+	cout << "Enter the starting position: "; cin >> start;
+	cout << "Enter the offset: "; cin >> offset;
 
-
+	//Call charachter and catch any exceptions
+	try
+	{
+		target = character(start, offset);
+		cout << "The target is: " << target;
+	}
+	catch (string invalidCharacterException)
+	{
+		cout << invalidCharacterException;
+	}
+	catch (string invalidRangeException)
+	{
+		cout << invalidRangeException;
+	}
+	catch (string invalidTransitionException)
+	{
+		cout << invalidTransitionException;
+	}
+	
 	return 0;
 }
 
@@ -19,3 +42,6 @@ char character(char start, int offset)
 {
 
 }
+
+//A-Z = 65-90
+//a-z = 97-122
