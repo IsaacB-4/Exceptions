2392464f5404f79391bac77f4807ca0cb5471fd0
Isaac Booth <TheRealIkeB@gmail.com>
2021-05-02 18:48:24 -0400

    Added a basic outline for the program

---
+++ Exceptions/Exceptions.cpp
@@ -0,0 +1,21 @@
+//Isaac Booth 
+//CIS 1202 101
+//May 2, 2021
+
+#include <iostream>
+using namespace std;
+
+//Function prototype
+char character(char start, int offset);
+
+int main()
+{
+
+
+	return 0;
+}
+
+char character(char start, int offset)
+{
+
+}
